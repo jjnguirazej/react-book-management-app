59ddfa938ea9c5c625c98fbe819d1d5e584d974f
Jorge Nguiraze <jjorge@technoplus.co.mz>
2022-01-07 16:07:00 +0200

    Initial comit.... Formatacao de pastas e components

---
+++ src/router/AppRouter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from '../components/Header';
+import AddBook from '../components/AddBook';
+import BooksList from '../components/BooksList';
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <div className="main-content">
+          <Routes>
+            <Route element={<BooksList/>} path="/" />
+            <Route element={<AddBook/>} path="/add" />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
